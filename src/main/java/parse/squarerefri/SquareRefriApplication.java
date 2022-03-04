94d571574bab41661d30731d222f5b94c831c9c7
chappse6 <chappse6@gmail.com>
2022-03-04 16:33:22 +0900

    Initial commit

---
+++ src/main/java/parse/squarerefri/SquareRefriApplication.java
@@ -0,0 +1,13 @@
+package parse.squarerefri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareRefriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareRefriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/parse/squarerefri/SquareRefriApplicationTests.java
@@ -0,0 +1,13 @@
+package parse.squarerefri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareRefriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
