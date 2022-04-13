86a108d174237668c04cddf0a258df7f8e7124e8
chappse6 <chappse6@gmail.com>
2022-04-13 14:14:24 +0900

    cascade 커밋

--- src/main/java/parse/squarerefri/domain/manage/domain/Management.java
+++ src/main/java/parse/squarerefri/domain/manage/domain/Management.java
@@ -21,7 +21,7 @@ public class Management {
     @Column(name = "manage_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "member_id")
     private Member member;
 
