477c8bb6229caf9953a041a5447b37fd7794c9c2
chappse6 <chappse6@gmail.com>
2022-05-05 20:23:59 +0900

    식품관리 주석 추가

--- src/main/java/parse/squarerefri/domain/manage/service/ManageService.java
+++ src/main/java/parse/squarerefri/domain/manage/service/ManageService.java
@@ -9,11 +9,23 @@ import java.util.List;
 
 public interface ManageService {
 
+    /**
+     * 식품관리 등록
+     */
     String registManage(ManageInput parameter);
 
+    /**
+     * 식품관리 전체 조회
+     */
     List<Management> findAll(String memberId, StorageStatus storageStatus);
 
+    /**
+     * 제품 종류 리스트를 위한 식품관리 전체 조회
+     */
     List<String> findAllForList();
 
+    /**
+     * 식품관리 삭제
+     */
     void deleteManage(Long id);
 }
